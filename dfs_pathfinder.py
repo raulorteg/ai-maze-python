4bfcf83135be3de329f3cbeca3e931015c172708
Raul <s202489@student.dtu.dk>
2021-05-13 15:52:33 +0200

    backtrack solution :sparkles:

--- dfs_pathfinder.py
+++ dfs_pathfinder.py
@@ -31,91 +31,90 @@ def is_in_map(pos, grid_dim):
 
 # ===========================
 
-def possible_next_steps(grid_dim, last_pos):
-    """
-    Parameters
-    ----------
-    grid_dim : tuple of 2 ints
-        dimensions of the grid
-    last_pos : tuple of 2 ints
-        x, y coordinates of current position
-    Returns
-        possible_steps: list of list of tuples (x,y) denoting the
-        next 2 movements possible in every direction possible
-    """
-    x, y = last_pos
-    movements = [(1,0), (-1,0), (0,1), (0,-1)]
-    possible_steps = []
-    for movement in movements:
-        dx, dy = movement
-        next_mov = (x+dx, y+dy)
-        if is_in_map(next_mov, grid_dim):
-            possible_steps.append(next_mov)
-        
-    return possible_steps
+class Node:
+    def __init__(self, pos, parent):
+        self.x = pos[0]
+        self.y = pos[1]
+        self.parent = parent
 
-def generate_step(grid, last_pos, pos_history, back_step):
-    """
-    Parameters
-    ----------
-    grid : list of list of ints
-        the grid, it is filled with 0, 1, 2, 3 that correspond
-        to different colors
-    last_pos : tuple of 2 ints
-        x, y coordinates of current position
-    pos_history : list of tuples of 2 ints
-        coordinates of last visited nodes, only add when see for the
-        first time
-    Returns
-        changes grid[x][y] to white through the path the algorithm is going
-        and paints the last_pos on the grid blue
-        returns grid, last_pos, back_step, done
-    """
-    x, y = last_pos
-    grid_dim = (len(grid)-1, len(grid[0])-1)
-    possible_steps = possible_next_steps(grid_dim, last_pos)
-    
-    valid_steps = []
-    for possible_step in possible_steps:
-        x, y = possible_step
-        if (grid[x,y] == 3): # if red
-            valid_steps = []
-            valid_steps.append(possible_step)
-            done = True
-            return grid, last_pos, back_step, done
-        
-        elif (grid[x,y] == 1): # if white
-            valid_steps.append(possible_step)
-            
-    if len(valid_steps) == 0:
-        last_pos = pos_history[-1-back_step]
-        back_step += 1
-        done = False
-        return grid, last_pos, back_step, done
-    
-    elif len(valid_steps) == 1:
-        back_step = 0
-        x, y = valid_steps[0]
-        last_pos = (x, y)
-        grid[x,y] = 4 # paint blue
-        done = False
-        return grid, last_pos, back_step, done
-    
-    else:
-        back_step = 0
-        index = randint(0, len(valid_steps))
-        last_pos = valid_steps[index]
-        x, y = last_pos
-        grid[x,y] = 4 # paint blue
-        done = False
-        return grid, last_pos, back_step, done
+    def __getitem__(self):
+          return (self.x, self.y)
 
+    def position(self):
+        return (self.x, self.y)
+
+
+class Queue:
+    def __init__(self):
+        self.Queue = []
+
+    def __len__(self):
+        return len(Queue)
+
+    def append(self, node):
+        self.Queue.append(node)
+
+    def get_node(self):
+        return self.Queue[-1]
+
+    def get_node_position(self):
+        node = self.Queue[-1]
+        return node.position()
+
+    def remove(self):
+        self.Queue = self.Queue[0:len(self.Queue)-1]
+
+class DFS:
+    def __init__(self, start_pos, goal_pos, grid_dim):
+        self.grid_dim = grid_dim
+        self.start_pos = start_pos
+        self.goal_pos = goal_pos
+        self.explored = Queue()
+        self.frontier = Queue()
+        node = Node(pos=start_pos, parent=None)
+        self.frontier.append(node)
+
+    def backtrack_solution(self, grid, curr_node):
+        solution = []
+        while curr_node.parent != None:
+            solution.append(curr_node.position())
+            curr_node = curr_node.parent
+
+        return solution
+
+    def compute_successors(self, grid):
+        curr_node = self.frontier.get_node()
+        x, y = curr_node.position()
+
+        buffer_nodes = []
+        for movement in [(1,0), (-1,0), (0,1), (0,-1)]:
+            dx, dy = movement
+            new_pos = (x+dx, y+dy)
+
+            if (is_in_map(new_pos, self.grid_dim)) and (grid[new_pos[0], new_pos[1]] in [1, 3]) :
+                new_node = Node(pos=new_pos, parent=curr_node)
+                buffer_nodes.append(new_node)
+                
+
+                if set(new_pos) == set(goal_pos):
+                    self.explored.append(self.frontier.get_node())
+                    self.frontier.remove()
+                    done = True
+                    solution = self.backtrack_solution(grid, curr_node=new_node)
+                    return solution, done
+
+        self.explored.Queue.append(curr_node)
+        self.frontier.remove()
+
+        [self.frontier.Queue.append(new_node) for new_node in buffer_nodes]
+        done = False
+        return [], done
 
 if __name__ == "__main__":
   start_t0 = time.time()
 
   # parsing user input
-  # example: python dfs_generator.py --display=True --maze_file=maze_1.csv
+  # example: python dfs_pathfinder.py --display=True --maze_file=maze_1.csv
   parser = argparse.ArgumentParser()
   parser.add_argument("--display", help="Display generating process 0: False, 1:True", default=1, type=int)
   parser.add_argument("--maze_file", help="filename (csv) of the maze to load.", default="maze_1.csv", type=str)
@@ -127,31 +126,32 @@ if __name__ == "__main__":
   num_columns = len(grid[0])
 
   # define start, define goal
-  start = (0,0)
-  last_pos = (0,0)
-  goal = (num_rows-1, num_columns-1)
-  pos_history = []
-  pos_history.append(last_pos)
-  back_step=0
-
-  # define the two colors of the grid RGB
-  black = (0, 0, 0) # grid == 0
-  white = (255, 255, 255) # grid == 1
-  green = (50,205,50) # grid == 2
-  red = (255,99,71) # grid == 3
-  grey = (211,211,211) # for background
-  blue = (153,255,255) # grid[x][y] == 4, where current position is
-
-  # set the height/width of each location on the grid
-  height = 7
-  width = height # i want the grid square
-  margin = 1 # sets margin between grid locations
+  start_pos = (0,0)
+  goal_pos = (num_rows-1, num_columns-1)
 
   # define start and goal
   grid[0, 0] = 2
   grid[-1, -1] = 3
 
+  grid_dim = (num_rows-1, num_columns-1)
+
   if args.display == 1:
+    # define the two colors of the grid RGB
+    black = (0,0,0)
+    white = (255, 255, 255)
+    green = (50,205,50)
+    red = (255,99,71)
+    grey = (211,211,211)
+    blue = (153,255,255)
+    magenta = (255,0,255)
+
+    idx_to_color = [black, white, green, red, blue, magenta]
+
+    # set the height/width of each location on the grid
+    height = 7
+    width = height # i want the grid square
+    margin = 1 # sets margin between grid locations
+
     # initialize pygame
     pygame.init()
 
@@ -159,9 +159,7 @@ if __name__ == "__main__":
     WINDOW_SIZE = [330, 330]
     screen = pygame.display.set_mode(WINDOW_SIZE)
 
-    pygame.display.set_caption(f"Pathfinder. {address}")
-
-    idx_to_color = [black, white, green, red, blue]
+    pygame.display.set_caption(f"DFS Pathfinder. Solving: {address}")
 
     # loop until done
     done = False
@@ -169,6 +167,8 @@ if __name__ == "__main__":
     close = False
 
     clock = pygame.time.Clock() # to manage how fast the screen updates
+
+    dfs = DFS(start_pos=start_pos, goal_pos=goal_pos, grid_dim=grid_dim)
 
     # main program
     while not done:
@@ -180,7 +180,6 @@ if __name__ == "__main__":
             elif event.type == pygame.KEYDOWN:
                 run = True
         
-        
         screen.fill(grey) # fill background in grey
         
         for row in range(num_rows):
@@ -191,19 +190,42 @@ if __name__ == "__main__":
                                   (margin + height) * row + margin,
                                   width, height])
         
-        # set limit to 60 frames per second
-        clock.tick(60)
         
-        # update screen
-        pygame.display.flip()
+        clock.tick(60) # set limit to 60 frames per second
+        pygame.display.flip() # update screen
         
         if run == True:
-            # feed the algorithm the last updated position and the grid
-            grid, last_pos, back_step, done = generate_step(grid, last_pos, pos_history, back_step)
-            if last_pos not in pos_history:
-                pos_history.append(last_pos)
+
             sleep(0.01)
+            solution, done = dfs.compute_successors(grid=grid)
         
+            explored = [node.position() for node in dfs.explored.Queue]
+
+            for pos in explored:
+                grid[pos[0], pos[1]] = 4
+
+    if done == True:
+        for pos in solution:
+            grid[pos[0], pos[1]] = 5
+        grid[0, 0] = 2
+        grid[-1, -1] = 3
+
+        screen.fill(grey) # fill background in grey
+       
+        for row in range(num_rows):
+            for column in range(num_columns):
+                color = idx_to_color[grid[row, column]]
+                pygame.draw.rect(screen, color, 
+                                [(margin + width) * column + margin, 
+                                (margin + height) * row + margin,
+                                width, height])
+        
+        
+        clock.tick(60) # set limit to 60 frames per second
+        pygame.display.flip() # update screen
+
+            
+    print("Solved! Click exit.")
     while not close:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -218,13 +240,24 @@ if __name__ == "__main__":
 
     # loop until done
     done = False
+    dfs = DFS(start_pos=start_pos, goal_pos=goal_pos, grid_dim=grid_dim)
 
     # main program
     while not done:
-        # feed the algorithm the last updated position and the grid
-        grid, last_pos, back_step, done = generate_step(grid, last_pos, pos_history, back_step)
-        if last_pos not in pos_history:
-              pos_history.append(last_pos)
+        solution, done = dfs.compute_successors(grid=grid)
+
+        explored = [node.position() for node in dfs.explored.Queue]
+
+        for pos in explored:
+            grid[pos[0], pos[1]] = 4
+
+    # lets save result in csv
+    for pos in solution:
+        grid[pos[0], pos[1]] = 5
+
+    grid[0, 0] = 2
+    grid[-1, -1] = 3
+
 
   # export maze to .csv file
   with open(f"mazes_solutions/dfs_{args.maze_file}", "w", newline="") as f:
